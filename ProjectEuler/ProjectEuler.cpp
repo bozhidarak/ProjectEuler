e64590715c5d3e347e33d41a6a00f50e67825a62
bozhidarak <72149764+bozhidarak@users.noreply.github.com>
2022-06-20 22:40:15 +0300

    add task 1 solution

--- ProjectEuler/ProjectEuler.cpp
+++ ProjectEuler/ProjectEuler.cpp
@@ -3,18 +3,20 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int sumOfThreeOrFiveDivisor(int num) { //task1
+	int sum = 0;
+	for (size_t i = 0; i < num; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	return sum;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main()
+{
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	std::cout << sumOfThreeOrFiveDivisor(1000);
+}
